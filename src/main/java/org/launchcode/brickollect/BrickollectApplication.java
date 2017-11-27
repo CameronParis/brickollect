ff501ccd677b0aaf2b6cb3d8e0b585b00b1f45de
Cameron Paris <Cameron.Paris@gmail.com>
2017-11-27 13:36:42 -0600

    Initial Commit

---
+++ src/main/java/org/launchcode/brickollect/BrickollectApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.brickollect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickollectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickollectApplication.class, args);
+	}
+}
